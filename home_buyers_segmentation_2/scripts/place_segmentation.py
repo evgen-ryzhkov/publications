7d3bab00387f3a7c1cb08077acc61f1b7749c407
Evgeniy <evgen.ryzhkov@frondevo.com>
2020-07-02 11:23:24 +0300

    - real data :: place segmentation - neighborhood creation

--- home_buyers_segmentation_2/scripts/place_segmentation.py
+++ home_buyers_segmentation_2/scripts/place_segmentation.py
@@ -4,69 +4,110 @@ from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 import matplotlib.pyplot as plt
 import seaborn as sns
 from .k_means_segmentation import get_number_of_segments, get_segments, validate_cluster_sizes
+from settings.secrets import API_KEY
 
 
 def get_place_segments(df_original):
+
+    # print(len(df_original.loc[pd.isna(df_original['location'])]))
+
     df_place_data = df_original[['city', 'neighborhood', 'location']].copy()
-    df_preprocessed = _preprocess_data(df_place_data)
+    # df_preprocessed = _preprocess_data(df_place_data)
 
     # to define number of cluster, run this function
     # get_number_of_segments(df_preprocessed)
-    num_clusters = 5
-    place_cluster_column_name = 'cluster_place'
-    df_original_place_segmented = get_segments(df_place_data, df_preprocessed, place_cluster_column_name, num_clusters)
-    f_validation, df_stat = validate_cluster_sizes(df_original_place_segmented, place_cluster_column_name)
-
-    _profile_clusters(df_original_place_segmented, df_stat, place_cluster_column_name, num_clusters)
+    # num_clusters = 5
+    # place_cluster_column_name = 'cluster_place'
+    # df_original_place_segmented = get_segments(df_place_data, df_preprocessed, place_cluster_column_name, num_clusters)
+    # f_validation, df_stat = validate_cluster_sizes(df_original_place_segmented, place_cluster_column_name)
+    #
+    # _profile_clusters(df_original_place_segmented, df_stat, place_cluster_column_name, num_clusters)
 
 
 def _preprocess_data(df):
     print('[INFO] Place data preprocessing started...')
-    df_custom_features = _create_custom_features(df)
+
+    _create_neighborhood_csv(df)
+    exit()
+    # df_custom_features = _create_custom_features(df)
 
     # choosing meaningful features for segmentation
     '''
         - cat_city/suburbs - different lifestyle
         - loc_[different location attributes] - lifestyle
     '''
-    df_for_segmentation = df_custom_features[['cat_city', 'cat_suburbs', 'loc_quiet_road', 'loc_busy_road',
-                                              'loc_in_center', 'loc_res_district', 'loc_near_park',
-                                              'loc_near_forest', 'loc_near_water', 'loc_rural']]
+    # df_for_segmentation = df_custom_features[['cat_city', 'cat_suburbs', 'loc_quiet_road', 'loc_busy_road',
+    #                                           'loc_in_center', 'loc_res_district', 'loc_near_park',
+    #                                           'loc_near_forest', 'loc_near_water', 'loc_rural']]
+    df_for_segmentation = df_custom_features[['cat_city', 'cat_suburbs', 'loc_quiet', 'loc_nature', 'loc_walkability']]
     print('[OK] Place data preprocessing finished.')
     return df_for_segmentation
 
+
+def _create_neighborhood_csv(df):
+    '''
+        using neighborhood names and Google Distance Matrix Api
+        get more useful information about neighbohoods
+    '''
+    df_neigborhoods = df[['city', 'neighborhood']].drop_duplicates()
+
+    df_neigborhoods['commit_time_car'] = ''
+    df_neigborhoods['commit_time_transit'] = ''
+
+    API_URL = 'http://maps.googleapis.com/maps/api/distancematrix/json?' + \
+              'language=en-EN' + \
+              '&mode=driving'
+    COUNTRY_NAME = 'Netherlands'
+    API_KEY_S = '@key=' + API_KEY
+
+
+
+    for index, row in df.iterrows():
+        print(row['city'], row['neighborhood'])
+
+    print(df_neigborhoods)
 
 def _create_custom_features(df):
     # in the city or suburbs
     df.loc[df['city'] == 'Amsterdam', 'cat_city'] = 1
     df.loc[df['city'] != 'Amsterdam', 'cat_suburbs'] = 1
 
-    # get unique values for feature analysis
-    # excluding nan
-    loc_values_list =df['location'].loc[pd.notna(df['location'])].unique().tolist()
+    # print(df['city'].unique())
 
-    # join into one string in order to convert back into list but in our conditions
-    separator = ', '
-    loc_values_string = separator.join(loc_values_list)
-    loc_values_string_without_and = loc_values_string.replace(' and', ',')
-
-    # convert string into list and get unique values
-    loc_values_list_2 = loc_values_string_without_and.split(', ')
-    loc_values_numpy = np.array(loc_values_list_2)
+    # # get unique values for feature analysis
+    # # excluding nan
+    # loc_values_list =df['location'].loc[pd.notna(df['location'])].unique().tolist()
+    #
+    # # join into one string in order to convert back into list but in our conditions
+    # separator = ', '
+    # loc_values_string = separator.join(loc_values_list)
+    # loc_values_string_without_and = loc_values_string.replace(' and', ',')
+    #
+    # # convert string into list and get unique values
+    # loc_values_list_2 = loc_values_string_without_and.split(', ')
+    # loc_values_numpy = np.array(loc_values_list_2)
+    #
+    # print(np.unique(loc_values_numpy))
 
     # custom creating onehot encoding for location
-    df.loc[df['location'].str.contains(pat='quiet road', na=False, case=False), 'loc_quiet_road'] = 1
-    df.loc[df['location'].str.contains(pat='busy road', na=False, case=False), 'loc_busy_road'] = 1
-    df.loc[df['location'].str.contains(pat='in center', na=False, case=False), 'loc_in_center'] = 1
-    df.loc[df['location'].str.contains(pat='residential district', na=False, case=False), 'loc_res_district'] = 1
-    df.loc[df['location'].str.contains(pat='alongside park', na=False, case=False), 'loc_near_park'] = 1
-    df.loc[df['location'].str.contains(pat='forest|wooded surroundings', na=False, case=False), 'loc_near_forest'] = 1
-    df.loc[df['location'].str.contains(pat='water|waterway|seaview', na=False, case=False), 'loc_near_water'] = 1
-    df.loc[df['location'].str.contains(pat='rural', na=False, case=False), 'loc_rural'] = 1
+    # df.loc[df['location'].str.contains(pat='quiet road', na=False, case=False), 'loc_quiet_road'] = 1
+    # df.loc[df['location'].str.contains(pat='busy road', na=False, case=False), 'loc_busy_road'] = 1
+    # df.loc[df['location'].str.contains(pat='in center', na=False, case=False), 'loc_in_center'] = 1
+    # df.loc[df['location'].str.contains(pat='residential district', na=False, case=False), 'loc_res_district'] = 1
+    # df.loc[df['location'].str.contains(pat='alongside park', na=False, case=False), 'loc_near_park'] = 1
+    # df.loc[df['location'].str.contains(pat='forest|wooded surroundings', na=False, case=False), 'loc_near_forest'] = 1
+    # df.loc[df['location'].str.contains(pat='water|waterway|seaview', na=False, case=False), 'loc_near_water'] = 1
+    # df.loc[df['location'].str.contains(pat='rural', na=False, case=False), 'loc_rural'] = 1
+
+    df.loc[df['location'].str.contains(pat='quiet road|rural', na=False, case=False), 'loc_quiet'] = 1
+    df.loc[df['location'].str.contains(pat='alongside park|forest|wooded surroundings|water|waterway|seaview', na=False, case=False), 'loc_nature'] = 1
+    df.loc[df['location'].str.contains(pat='in center|residential district', na=False, case=False), 'loc_walkability'] = 1
 
     # fill nan for new custom features
-    nan_columns = ['cat_city', 'cat_suburbs', 'loc_quiet_road', 'loc_busy_road', 'loc_in_center', 'loc_res_district', 'loc_near_park',
-                   'loc_near_forest', 'loc_near_water', 'loc_rural']
+    # nan_columns = ['cat_city', 'cat_suburbs', 'loc_quiet_road', 'loc_busy_road', 'loc_in_center', 'loc_res_district', 'loc_near_park',
+    #                'loc_near_forest', 'loc_near_water', 'loc_rural']
+
+    nan_columns = ['cat_city', 'cat_suburbs', 'loc_quiet', 'loc_nature', 'loc_walkability']
     for col in nan_columns:
         df[col] = df[col].fillna(0)
 
@@ -76,9 +117,11 @@ def _create_custom_features(df):
 def _profile_clusters(df_segmented, df_stat, cluster_col_name, n_clusters):
     # define value for calculation distribution for each column
     loc_types = ['cat_city', 'cat_suburbs',]
-    loc_properties = ['loc_quiet_road', 'loc_busy_road', 'loc_in_center', 'loc_res_district', 'loc_near_park',
-                   'loc_near_forest', 'loc_near_water', 'loc_rural']
+    # loc_properties = ['loc_quiet_road', 'loc_busy_road', 'loc_in_center', 'loc_res_district', 'loc_near_park',
+    #                'loc_near_forest', 'loc_near_water', 'loc_rural']
+    loc_properties = ['loc_quiet', 'loc_nature', 'loc_walkability']
     df_profiling_cols = loc_types + loc_properties
+
 
     # initiation of df_profiling
     n_columns = len(df_profiling_cols)
